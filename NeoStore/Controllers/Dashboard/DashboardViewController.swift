efdb64e95cd62f34526ec6e80481fa5488a027bf
Bhushan Mokal <bhushan.mokal@neosoftmail.com>
2022-05-16 18:08:26 +0530

    removed storyboard

---
+++ NeoStore/Controllers/Dashboard/DashboardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DashboardViewController.swift
+//  NeoStore
+//
+//  Created by Neosoft on 16/05/22.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Dashborad Screen appear.")
+        view.backgroundColor = .yellow
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NeoStore/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  NeoStore
+//
+//  Created by Neosoft on 16/05/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Login Screen appear.")
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
